d6cc4ca05329cad0bd00d334a50eab1d147a35d9
Ayush Gupta <garenaraidergaming@gmail.com>
2022-04-06 07:43:37 +0530

    Added navbar and funny functions...

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello, Welcome please wait untill the site is completed..');
+
+document.getElementById('menu').addEventListener('click', function () {
+    console.log('The Navbar on mobile was opened!');
+});
+
+function chtitle() {
+    x = prompt('Enter the text you want to select as title...');
+    document.title = x;
+}
